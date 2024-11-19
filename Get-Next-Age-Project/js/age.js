482e10407db2fe956c5a4d829db60b081b5fb086
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-19 14:59:47 +0100

    a=handle empty string age

--- Get-Next-Age-Project/js/age.js
+++ Get-Next-Age-Project/js/age.js
@@ -2,5 +2,8 @@
  * @param {string} age
  */
 function getNextAge(age) {
-    return 1 + Number.parseInt(age, 10)
+  if (age === "") {
+    return 0;
+  }
+  return 1 + Number.parseInt(age, 10)
 }
--- Get-Next-Age-Project/js/index.js
+++ Get-Next-Age-Project/js/index.js
@@ -2,5 +2,5 @@ const age = document.querySelector("#your-age");
 const nextAge = document.querySelector("#next-age");
 
 age.addEventListener("keyup", () => {
-    nextAge.textContent = getNextAge(age.value);
+  nextAge.textContent = getNextAge(age.value);
 });
